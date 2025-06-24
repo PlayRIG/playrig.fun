06a462df343dc55d98ac0824e57f0f29556e99b2
Rahxephonz <mynev.id@gmail.com>
2025-06-24 11:54:00 +0700

    feat: adding railway config deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,26 @@ import {defineConfig} from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import svgr from "vite-plugin-svgr";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  server: {
+    port: 8080,
+    host: "0.0.0.0",
+  },
   integrations: [react(), tailwind({nesting: true})],
   experimental: {
     svg: true,
   },
   vite: {
+    define: {
+      global: "globalThis",
+    },
     plugins: [
       svgr({
         include: "**/*.svg?react",
