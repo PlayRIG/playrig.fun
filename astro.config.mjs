9158dcc98b32bb8adb9545cc6a218e54da0de0c4
Rahxephonz <mynev.id@gmail.com>
2025-06-30 18:28:34 +0700

    chore: change deployment platform and revert config astro and script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,11 @@
 // @ts-check
-// @ts-check
 import {defineConfig} from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import svgr from "vite-plugin-svgr";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
-  server: {
-    port: 8080,
-    host: "0.0.0.0",
-  },
   integrations: [react(), tailwind({nesting: true})],
   experimental: {
     svg: true,
